1b641641305561b205a91e1d308461c135897895
Prem <premnsa108@gmail.com>
2024-10-22 10:59:23 +0545

    a

--- Server/index.js
+++ Server/index.js
@@ -31,6 +31,7 @@ app.use(cors({
     origin: function (origin, callback) {
         const allowedOrigins = [
             'https://kppo-frontend.onrender.com',
+            'https://kppo-frontend.onrender.com/',
             'http://localhost:5173',
             'http://192.168.162.15:8211',
             'http://192.168.1.35:5173',
