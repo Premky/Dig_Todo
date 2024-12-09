7322510d26f5f166fe3cce3c3cb056fe7d96c75f
Prem Kumar Yadav <premnsa108@gmail.com>
2024-12-09 16:09:00 +0545

    4.08

--- Frontend/src/Components/PrivateRoute/EmpRoute.jsx
+++ Frontend/src/Components/PrivateRoute/EmpRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const EmpRoute = () => {
-    const branch = localStorage.getItem('bid');
+    const branch = localStorage.getItem('branch');
     const isValidUser = localStorage.getItem("valid") && (branch === 'कर्मचारी प्रशासन');
     return isValidUser ? <Outlet /> : <Navigate to="/" />;
 };
--- Frontend/src/Components/PrivateRoute/TangoRoute.jsx
+++ Frontend/src/Components/PrivateRoute/TangoRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const TangoRoute = () => {
-    const userType = localStorage.getItem('bid');
+    const userType = localStorage.getItem('branch');
     // console.log(userType)
     const isValidUser = localStorage.getItem("valid") && (userType === 'ट्राफिक');
     return isValidUser ? <Outlet /> : <Navigate to="/" />;
