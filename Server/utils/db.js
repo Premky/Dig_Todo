fdee41cc1890bbaa362902b52eef0b4e69aad97c
Prem Kumar Yadav <premnsa108@gmail.com>
2024-12-17 13:24:03 +0545

    ll

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -2,23 +2,25 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const con = mysql.createConnection({
-    host:'localhost',
-    user:'software',    
-    password:'Mark3@Rifile',
-    database:'kppo_program'
-})
-
 // const con = mysql.createConnection({
-//     host:process.env.DB_HOST,
-//     port:process.env.DB_PORT,    
-//     user:process.env.DB_USER,    
-//     password:process.env.DB_PASSWORD,
-//     database:process.env.DB_NAME,
-//     ssl:{
-//         rejectUnauthorized:false,
-//     }
+//     host:'192.168.162.15',
+//     port:'3306',
+//     user:'software',    
+//     password:'Mark3@Rifile',
+//     database:'kppo_program'
 // })
+
+const con = mysql.createConnection({
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,    
+    user:process.env.DB_USER,    
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    ssl:{
+        rejectUnauthorized:false,
+    }
+})
+console.log(con)
 
 con.connect(function(err){
     if(err){
